a2ad0486d55b702748a653762254f2991d5760df
Max <maxprizrac@gmail.com>
2026-08-06 11:50:35 +0300

    Integration temp mails (#1)
    * Мелкие правки в классах отвечающих за временную почту
    
    * Интеграция нового SDK temp-mails с поддержкой различных api

--- mail/TempMailManager.py
+++ mail/TempMailManager.py
@@ -1,39 +1,39 @@
-from tempmail import AsyncTempMailClient, EmailAddress
-from resources.ResourcesManager import ResourcesManager
-
+from temp_mails import Tenminemail_com
 from loguru import logger
-from typing import final, Optional, ClassVar
+from typing import final, Optional
 
 
 @final
 class TempMailManager:
 
-    __client: ClassVar[AsyncTempMailClient]
-
-    @classmethod
-    def init_client(cls) -> None:
-        cls.__client = AsyncTempMailClient(api_key=ResourcesManager.AI_API_KEY)
-
-    @classmethod
-    async def make_mail(cls) -> Optional[EmailAddress]:
+    def __init__(self) -> None:
         try:
-            async with cls.__client as client:
-                email = await client.create_email()
-                return email
+            self.email = Tenminemail_com()
         except Exception as error:
-            logger.error(f"Ошибка создания временной почты {error}")
-        return None
+            logger.info(f"Ошибка инициализации временой почты: {error}")
 
-    @classmethod
-    async def get_mails(cls, email: EmailAddress) -> Optional[str]:
+    def get_inbox(self) -> Optional[str]:
         try:
-            async with cls.__client as client:
-                result = ""
-                messages = await client.list_email_messages(email.email)
-                if messages:
-                    for msg in messages:
-                        result += (f"📧От кого:\n{msg.from_addr}\n\n📝Тема:\n{msg.subject}\n\n\n")
-                    return result
+            result = ""
+            messages = self.email.get_inbox()
+            if messages:
+                for msg in messages:
+                    from_addr = msg.get('from', 'Неизвестно')
+                    subject = msg.get('subject', 'Пусто')
+                    msg_id = msg.get('id')
+
+                    try:
+                        if msg_id:
+                            content = self.email.get_mail_content(mail_id=msg_id)
+                            if content:
+                                if len(content) > 500:
+                                    content = content[:500] + "...\n(текст обрезан)"
+                                result += (f"📧От кого:\n{from_addr}\n📝Тема:\n{subject}\n\n📜Содержание:\n{content}")
+
+                    except Exception as error:
+                        logger.error(f"Не удалось получить содержимое письма: {error}")
+
+                return result
         except Exception as error:
             logger.error(f"Ошибка при получении писем: {error}")
-        return None
+        return "Пусто"
