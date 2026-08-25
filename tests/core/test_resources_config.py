27910e2be41c038c50b8627bcf7982a3a2970abc
whgds1360 <maxprizrac@gmail.com>
2026-08-25 10:30:08 +0300

    Добавил структуру для папки с тестами и реализовал тест для класса ресурсов

---
+++ tests/core/test_resources_config.py
@@ -0,0 +1,24 @@
+from pytest import fixture
+from pathlib import Path
+from core.resources_manager import ResourcesManager
+
+
+@fixture
+def load_test_data() -> Path:
+    return Path(__file__).parent.joinpath("configs_for_tests", "config.env")
+
+
+def test_resource_manager(load_test_data):
+    ResourcesManager.load_config(env_path=load_test_data)
+
+    assert (ResourcesManager.AI_API_KEY is not None
+            and ResourcesManager.AI_API_KEY != "")
+
+    assert (ResourcesManager.DB_URL is not None
+            and ResourcesManager.DB_URL != "")
+
+    assert (ResourcesManager.TEMP_MAIL_API_KEY is not None
+            and ResourcesManager.TEMP_MAIL_API_KEY != "")
+
+    assert (ResourcesManager.TG_TOKEN is not None
+            and ResourcesManager.TG_TOKEN != "")
